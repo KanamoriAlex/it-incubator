d9ff0935bd129456880b65d40a962492f9215da5
KanamoriAlex <kanamorialex@gmail.com>
2023-02-12 17:51:53 +1100

    Length

--- 12. String length.js
+++ 12. String length.js
@@ -1,7 +1,11 @@
-let lastNameLength = 0;
+/*let lastNameLength = 0;
 const lastName = "Lovelace";
 
-// Only change code below this line
+Only change code below this line
 lastNameLength = lastName.length;
-console.log(lastNameLength);
+console.log(lastNameLength); */
+
+let lastName = "Lovelace";
+let thirdLetterOfLastName = lastName[2]; // Change this line
+
 

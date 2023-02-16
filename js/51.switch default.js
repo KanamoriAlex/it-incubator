c08fde19204b1788451dd15226039f454661d11e
KanamoriAlex <kanamorialex@gmail.com>
2023-02-17 06:34:35 +1100

    switch default

---
+++ js/51.switch default.js
@@ -0,0 +1,24 @@
+function switchOfStuff(val) {
+  var answer = "";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+    return answer;  
+  }
+
+  /**Write a switch statement to set answer for the following conditions:
+a - apple
+b - bird
+c - cat
+default - stuff */

39169325caf745d71097711a6447f31456d77570
KanamoriAlex <kanamorialex@gmail.com>
2023-02-17 07:04:36 +1100

    ...

---
+++ js/54.If trick.js
@@ -0,0 +1,22 @@
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+console.log(isLess(40, 15));
+
+isLess(10, 15);
+
+/**Fix the function isLess to remove the if/else statements.
+  function isLess(a, b) {
+  // Only change code below this line
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+
+isLess(10, 15); */
